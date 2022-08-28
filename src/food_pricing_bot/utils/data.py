dd2aa06a863eb62c1b3153c523b52f46d9a3d153
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-08-28 13:28:37 +0200

    Implemented get_img and get_txt

--- src/food_pricing_bot/utils/data.py
+++ src/food_pricing_bot/utils/data.py
@@ -1,8 +1,20 @@
-from .storage import get_df
+from telegram import InputFile
+
+from .storage import get_df, get_img_path
 
 DF = get_df(split="test")
 
 
-async def get_correct_price(item_id: str) -> int:
+def get_correct_price(item_id: str) -> int:
     fractional_price = DF.loc[item_id, "fractional_price"]
     return fractional_price
+
+
+async def get_img(item_id: str) -> InputFile:
+    img_bytes = await open(get_img_path(item_id), mode="rb").read()
+    return InputFile(img_bytes)
+
+
+def get_txt(item_id: str) -> str:
+    txt = DF.loc[item_id, "txt"]
+    return txt
